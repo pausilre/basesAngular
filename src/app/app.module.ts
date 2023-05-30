8e3fda920f311cd2327ad8298809d51fbbfdc43e
psilvestre <psilvestre@iti.es>
2023-05-30 15:48:43 +0200

    bases de proyecto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HeroesModule } from './heroes/hero/heroes.module';
 import { AppComponent } from './app.component';
+import { CounterModule } from './counter/counter.module';
+import { DbzModule } from './dbz/dbz.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    CounterModule,
+    HeroesModule,
+    DbzModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
